eb4326ef87838e6f0cb84c53c286965077fddff7
Miroslav Hudec <mijujda@gmail.com>
2021-11-03 17:02:06 +0100

    Prep for 0.1.15

--- net_templates/templar.py
+++ net_templates/templar.py
@@ -2,25 +2,20 @@ import pathlib
 import dataclasses
 import jinja2
 from pydantic import BaseModel
-from pydantic.typing import Literal, Union, Dict, List
+from pydantic.typing import Literal, Union, Dict, List, Type
+
+from net_models.inventory import ConfigDefaults
 
 from net_templates.definitions import TEMPLATES_DIR
 from net_templates.filters import NetFilters
 
-supported_device_types = Literal['ios']
-
-
-class ConfigDefaultsBase(BaseModel):
-    INCLUDE_DEFAULTS: bool = None
-    PLATFORM_CDP_DEFAULT_ON: bool = None
-    INTERFACES_DEFAULT_NO_SHUTDOWN: bool = None
-    INTERFACE_DEFAULT_MODE: Literal['switched', 'routed'] = None
+SUPPORTED_DEVICE_TYPES = Literal['ios']
 
 
 class TemplarBase:
 
     @classmethod
-    def get_device_type_environment(cls, device_type: supported_device_types) -> jinja2.Environment:
+    def get_device_type_environment(cls, device_type: SUPPORTED_DEVICE_TYPES, defaults: Type[ConfigDefaults] = None) -> jinja2.Environment:
         env = jinja2.environment.Environment(
             loader=jinja2.loaders.FileSystemLoader(
                 searchpath=TEMPLATES_DIR.joinpath(device_type)
@@ -29,11 +24,13 @@ class TemplarBase:
             trim_blocks=True,
             lstrip_blocks=True,
             # undefined=jinja2.runtime.ChainableUndefined,
-            undefined=jinja2.runtime.StrictUndefined,
+            undefined=jinja2.runtime.StrictUndefined
         )
 
         net_filters = NetFilters().filters()
         env.filters.update(net_filters)
+        if defaults is not None:
+            env.globals.update(defaults)
         return env
 
     @classmethod
@@ -43,8 +40,9 @@ class TemplarBase:
         env.loader.searchpath.append(str(path))
 
     @classmethod
-    def render_template(cls, template_name: str, data: dict, device_type: supported_device_types = 'ios') -> str:
-        env = cls.get_device_type_environment(device_type=device_type)
+    def render_template(cls, template_name: str, data: dict, device_type: SUPPORTED_DEVICE_TYPES = 'ios', env: jinja2.Environment = None) -> str:
+        if env is None:
+            env = cls.get_device_type_environment(device_type=device_type)
         template = None
         try:
             template = env.get_template(name=template_name)
@@ -56,7 +54,7 @@ class TemplarBase:
         return result
 
 
-def get_template_dir(device_type: supported_device_types = 'ios'):
+def get_template_dir(device_type: SUPPORTED_DEVICE_TYPES = 'ios'):
     env = TemplarBase.get_device_type_environment(device_type=device_type)
     return pathlib.Path(env.loader.searchpath[0])
 
